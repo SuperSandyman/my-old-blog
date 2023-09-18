518aec1143333360f6bf16d3825f6871f05c0b8a
SuperSandyman <66544945+SuperSandyman@users.noreply.github.com>
2023-09-18 19:13:06 +0900

    robots update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 import urlJoin from "url-join";
 
-const baseURL = process.env.BASE_URL || "";
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "";
 
 export default function robots(): MetadataRoute.Robots {
     return {
