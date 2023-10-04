3baf8c0813d0f43863a1f75c30943353bcae3334
SuperSandyman <66544945+SuperSandyman@users.noreply.github.com>
2023-10-04 16:47:14 +0900

    sidebar update3

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -6,7 +6,7 @@ export const ShareButtons = ({ title, slug }) => {
             <button className=" bg-blue-500 py-2 px-4 rounded-lg inline-flex items-center">
                 <FaTwitter className="text-white mr-2" />
                 <a
-                    href={`https://twitter.com/intent/tweet?text=${title} https://www.sandyman.dev/posts/${slug}`}
+                    href={`https://twitter.com/intent/tweet?text=${title} | Sandyマンのブログ https://www.sandyman.dev/posts/${slug}`}
                     className="text-white font-bold"
                 >
                     Twitterでシェア！
